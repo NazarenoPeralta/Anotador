d84f1c767f3cf6d79bace8382f2ca77f410e7e20
Franco Nazareno Peralta <nazaperalta@outlook.com>
2022-01-19 11:49:47 -0300

    Primer funcion finalizada, formulario cantidad de jugadores

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ function pasoUno(){
     //ocultar formilario primerPaso y mostrar el segundo formulario
     var cantJugadores
     cantJugadores = document.getElementById("inputCantidad").value;
+    var $form1 = document.getElementById("formCantidad")
     
     for(let i = 0;i < cantJugadores; i++){
         //Crear inputs nombre segun cantidad de jugadores dentro del segundo form
-        document.querySelector("#formNombres").innerHTML = '<p class="segundoPaso">Nombre de los jugadores:</p> <input type="number" id="inputNombres" class="segundoPaso"> <button id="buttonNombres" class="segundoPaso"></button>'
+        const $ul = document.createElement("ul")
+        $ul.innerHTML = '<li class="segundoPaso">Nombre del jugador: ' + (i+1) + '</li> <input type="number" id="inputNombres" class="segundoPaso"> <button id="buttonNombres" class="segundoPaso"></button>'
+        document.getElementById("formNombres").appendChild($ul)
     }
+    $form1.classList.add("esconder")
 }
