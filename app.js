2237c2ffdca297e5cbf9f5d4d13f2a449cae791f
Franco Nazareno Peralta <nazaperalta@outlook.com>
2022-05-02 12:53:57 -0300

    Actualizacion de variables y nombres en la cabecera del anotador

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
 var cantJugadores;
 nombres = [];
 
-function pasoUno(){
-    //ocultar formilario primerPaso y mostrar el segundo formulario
+function CantidadJugadores(){
     cantJugadores = document.getElementById("inputCantidad").value;
-    var $form1 = document.getElementById("formCantidad")
-    
+    var $formCantidad = document.getElementById("formCantidad")
+
     for(let i = 0;i < cantJugadores; i++){
         //Crear inputs nombre segun cantidad de jugadores dentro del segundo form
-        const $ul = document.createElement("ul")
-        $ul.innerHTML = '<li class="segundoPaso">Nombre del jugador: ' + (i+1) + '</li> <input type="text" id="inputNombre'+ (i+1) + '" class="segundoPaso">'
-        document.getElementById("formNombres").appendChild($ul)
+        const $ulNombres = document.createElement("ul")
+        $ulNombres.innerHTML = '<li class="segundoPaso">Nombre del jugador: ' + (i+1) + '</li> <input type="text" id="inputNombre'+ (i+1) + '" class="segundoPaso">'
+        document.getElementById("formNombres").appendChild($ulNombres)
     }
-    const $div = document.createElement("div")
-    $div.innerHTML = '<button id="buttonNombres" type="button" class="segundoPaso" onclick="pasoDos()">Aceptar</button>'
-    document.getElementById("formNombres").appendChild($div)
+    const $buttonNombres = document.createElement("div")
+    $buttonNombres.innerHTML = '<button id="buttonNombres" type="button" class="segundoPaso" onclick="nombresTabla()">Aceptar</button>'
+    document.getElementById("formNombres").appendChild($buttonNombres);
 
-    $form1.classList.add("esconder")
+    $formCantidad.classList.add("esconder");
 }
 
-function pasoDos(){
-    var $formDatos = document.getElementById("formNombres")
+function nombresTabla(){
+    var $formNombres = document.getElementById("formNombres")
     
     for(let i = 0;i < cantJugadores; i++){
         nombres[i] = document.getElementById("inputNombre" + (i+1)).value;
-        console.log(nombres[i]);
     }
     
-    $formDatos.classList.add("esconder")
-    pasoTres();
+    $formNombres.classList.add("esconder")
+    mostrarTabla();
 }
 
-function pasoTres(){
-    const tablero = document.createElement("div");
-    tablero.innerHTML = '<div class="table"> <thead id="FilaJugadores">  <tr id="idNombres"> <th scope="col">#Mano</th> </tr> </thead>';
-    const $NombresTablero = document.getElementById("idNombres").value
-
+function mostrarTabla(){
+    var $tabla = document.getElementById("tabla")
+    
     for(let i = 0;i < cantJugadores; i++){
-        
-        $NombresTablero.innerHTML = '<th scope="col">'+ nombres[i] +'</th>'
+        var $headTable = [];
+        $headTable[i] = document.getElementById("Player"+ (i+1))
+        $headTable[i].innerText = nombres[i];
     }
+
+    $tabla.classList.remove("esconder")
 }
